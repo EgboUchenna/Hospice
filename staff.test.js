377319f5eda32e21ebd30f17e40d4d439a4bf7fb
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-26 14:07:26 +0100

    Add test to check for a staff readabilty by his ID.

--- staff.test.js
+++ staff.test.js
@@ -7,12 +7,23 @@ var Staff1 = new Staff(
   "egbouchenna001@gmail.com",
   "phoneix120"
 );
+
+var Staff2 = new Staff(
+    " Uchenna Egbo",
+    "uchenna001@gmail.com",
+    "phoneix140"
+  );
 console.log(db.Staff);
 
 //  Check if a staff object  was created successfully
 test('New Staff to be successfully created', function(){
     expect(Staff1).toEqual(db.Staff['1']);
-})
+});
+
+// Read a single staff by his ID and output the details.
+test("Read a single Staff by ID to and return his object", function() {
+    expect(Staff2.readSingleStaff(1)).toEqual(db.Staff["1"]);
+  });
 
 // test("Admin Doctor can create another staff", function() {
 //   var anotherStaff = Staff1.createNewStaff(
