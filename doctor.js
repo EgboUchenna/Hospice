2277ee46ac850e5ed3894b795a1e80fee7e11669
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-27 02:36:08 +0100

    Add method for admin Doctor to delete all Staff.

--- doctor.js
+++ doctor.js
@@ -1,9 +1,9 @@
-const Staff = require('./staff');
+const Staff = require("./staff");
 const db = require("./database");
 
 // Bind the Staff constructor to the Doctor
-function Doctor(name,email,password){
-    Staff.call(this,name,email,password);
+function Doctor(name, email, password) {
+  Staff.call(this, name, email, password);
 }
 
 // Copies the Staff prototype but creates a new Reference
@@ -14,16 +14,22 @@ Doctor.prototype.constructor = Doctor;
 
 // Add all methods that can be carried out by the admin Doctor to its prototype.
 Doctor.prototype.createNewStaff = function(name, email, password) {
-    return new Staff(name, email, password);
-  };
-  
+  return new Staff(name, email, password);
+};
+
 // Read all Staff  from database.
 Doctor.prototype.readAllStaff = function() {
-    return db.Staff;
-}
+  return db.Staff;
+};
 
+// Delete a single Staff from database.
 Doctor.prototype.deleteStaff = function(staff_id) {
-    return delete db.Staff[staff_id.toString()];
-  };
+  return delete db.Staff[staff_id.toString()];
+};
+
+//  Deleting all Staff from the database
+Doctor.prototype.deleteAllStaff = function() {
+  return (db.Staff = {});
+};
 
 module.exports = Doctor;
