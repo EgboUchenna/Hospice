6ec3cea1ba016f3f040dad4718c1d187191bd383
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-27 02:18:57 +0100

    Add test to check if admin Doctor can read all staff.

--- doctor.test.js
+++ doctor.test.js
@@ -4,9 +4,16 @@ const db = require("./database");
 //  Create an Admin Doctor from the Doctor constructor
 var adminDoctor = new Doctor("Muse", "handsum-Muse@gmail.com", "mainDoc1");
 
-//  Doctor admin can create a new user
+//  Admin Doctor can create a new user
 test("Doctor can create a new  Staff", function() {
     var newStaff = adminDoctor.createNewStaff("Uchenna", "uche@gmail.com", "uche");
     expect(newStaff.name).toBe("Uchenna");
     console.log(db.Staff);
   });
+
+//   Admin Doctor to read all Staff
+  test("Doctor to read all Staff", function() {
+    expect(adminDoctor.readAllStaff()).toEqual(db.Staff);
+  });
+  
+  
