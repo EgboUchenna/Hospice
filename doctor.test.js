7e7dd0de1470013b51d016a0c69e7fdb180cedb5
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-27 22:24:38 +0100

    Add test to check for admin Doctor can read all operations by staff.

--- doctor.test.js
+++ doctor.test.js
@@ -16,11 +16,19 @@ test("Doctor can create a new  Staff", function() {
     expect(adminDoctor.readAllStaff()).toEqual(db.Staff);
   });
   
-//   Admin Doctor to delete a single Staff
+//   Admin Doctor to delete a single Staff.
   test("Doctor can delete  a Staff", function() {
     expect(adminDoctor.deleteStaff(2)).toBeTruthy();
   });
 
+  //   Admin Doctor to delete all Staff.
   test("Doctor to delete all Staff", function() {
     expect(adminDoctor.deleteAllStaff()).toEqual({});
   });
+
+  //   Admin Doctor to read all Staff Operations.
+  test("Doctor to read all Staff Operations", function() {
+    expect(adminDoctor.readAllOperations()).toEqual(db.Operations);
+  });
+  
+  
